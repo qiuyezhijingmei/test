a4ceea9838335d7fed8ba35a86fb3f919bd58931
liyaming <liyaming@liyaming>
2017-08-21 17:06:07 +0800

    提交

---
+++ firstgit/src/com/qianfeng/User.java
@@ -0,0 +1,17 @@
+package com.qianfeng;
+
+/**
+
+ * @ClassName:     User.java
+
+ * @author          李亚明
+
+ * @Date           2017年8月21日 下午5:03:20 
+
+ */
+
+public class User {
+private int id;
+private String name;
+private String sex;
+}
