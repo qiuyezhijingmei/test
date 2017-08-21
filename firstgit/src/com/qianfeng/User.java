f910f4dd2b80938a61d924d0cf38d61362573cd2
liyaming <liyaming@liyaming>
2017-08-21 17:16:22 +0800

    第三次提交

--- firstgit/src/com/qianfeng/User.java
+++ firstgit/src/com/qianfeng/User.java
@@ -38,6 +38,10 @@ public int getAge() {
 }
 public void setAge(int age) {
 	this.age = age;
+}
+@Override
+public String toString() {
+	return "User [id=" + id + ", name=" + name + ", sex=" + sex + ", age=" + age + "]";
 };
 
 }
